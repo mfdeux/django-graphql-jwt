1123843bf1d5c28c0a287387f9da2096f24f2e7d
Marc Ford <mrfxyz567@gmail.com>
2019-08-15 15:46:24 -0400

    Add organization

--- graphql_jwt/settings.py
+++ graphql_jwt/settings.py
@@ -33,7 +33,7 @@ DEFAULTS = {
     ),
     'JWT_GET_USER_BY_NATURAL_KEY_HANDLER':
         'graphql_jwt.utils.get_user_by_natural_key',
-    'JWT_GET_ORGANIZATION BY_NATURAL_KEY_HANDLER':
+    'JWT_GET_ORGANIZATION_BY_NATURAL_KEY_HANDLER':
         'graphql_jwt.utils.get_organization_by_natural_key',
     'JWT_REFRESH_EXPIRED_HANDLER': 'graphql_jwt.utils.refresh_has_expired',
     'JWT_GET_REFRESH_TOKEN_HANDLER':
